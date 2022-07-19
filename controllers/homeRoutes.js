3a65a4c8778104282d5ba120943c79f5aad3b79f
Mister Tanatswa <tanatswasingano@gmail.com>
2022-07-19 19:24:04 -0400

    updated api routees and controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,10 +13,12 @@ router.get('/', async (req, res) => {
         },
       ],
     });
+
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
+
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('all-posts', { 
       posts, 
       logged_in: req.session.logged_in 
     });
@@ -24,6 +26,7 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -34,7 +37,9 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     });
+
     const post = postData.get({ plain: true });
+
     res.render('post', {
       ...post,
       logged_in: req.session.logged_in
@@ -43,6 +48,7 @@ router.get('/post/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
@@ -51,7 +57,9 @@ router.get('/profile', withAuth, async (req, res) => {
       attributes: { exclude: ['password'] },
       include: [{ model: Post }],
     });
+
     const user = userData.get({ plain: true });
+
     res.render('profile', {
       ...user,
       logged_in: true
@@ -60,12 +68,23 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
   }
+
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+})
+
 module.exports = router;
